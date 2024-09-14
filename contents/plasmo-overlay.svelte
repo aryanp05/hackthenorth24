50195036cdf709b595d920806ddaa15a37a21aa5
aaron <aaronye1001@gmail.com>
2024-09-14 11:05:50 -0400

    Init

---
+++ contents/plasmo-overlay.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+  import cssText from "data-text:~/contents/plasmo-overlay.css"
+  import type { PlasmoCSConfig } from "plasmo"
+
+  export const config: PlasmoCSConfig = {
+    matches: ["https://www.plasmo.com/*"]
+  }
+
+  export const getStyle = () => {
+    const style = document.createElement("style")
+    style.textContent = cssText
+    return style
+  }
+</script>
+
+<span class="hw-top">Every living thing wants to survive</span>
+
+<style>
+  .hw-top {
+    background: red;
+    color: white;
+    font-family: "Fascinate";
+    width: 200px;
+    border: 8px solid aqua;
+  }
+</style>
